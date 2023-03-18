5086d59e24323b7e7b722b69c0f206a164ce5f9e
Amey Naik <104484643+ameynaik-9@users.noreply.github.com>
2023-03-18 11:14:30 +0530

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void solve()
+{
+  
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    for (; t--;)
+    {
+       solve();
+    }
+    return 0;
+}
